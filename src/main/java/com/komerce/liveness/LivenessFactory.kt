d3f6e650dce01d0eec086b2279a1c3485bbc8d1e
Nunu Nugraha <nununugraha@Nunus-MacBook-Pro.local>
2025-12-11 16:08:57 +0700

    add codingan livenessfactory

--- src/main/java/com/komerce/liveness/LivenessFactory.kt
+++ src/main/java/com/komerce/liveness/LivenessFactory.kt
@@ -1,5 +1,26 @@
 package com.komerce.liveness
 
-class LivenessFactory {
+import android.content.Context
+import com.komerce.liveness.api.LivenessDetector
+import com.komerce.liveness.internal.MLKitFaceService
 
+/**
+ * LivenessFactory
+ *
+ * Pintu gerbang utama (Entry Point) untuk menggunakan SDK ini.
+ * Gunakan class ini untuk membuat instance dari [LivenessDetector].
+ */
+object LivenessFactory {
+
+    /**
+     * Membuat instance detektor liveness.
+     *
+     * @param context Context aplikasi (bisa Activity atau Application Context).
+     * @return Object yang mengimplementasikan interface [LivenessDetector].
+     */
+    fun create(context: Context): LivenessDetector {
+        // Di sini kita bikin instance dari implementation yang "Internal/Rahasia"
+        // dan mengembalikannya sebagai Interface "Public".
+        return MLKitFaceService(context)
+    }
 }
